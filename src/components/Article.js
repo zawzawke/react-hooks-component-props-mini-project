99409c687a3fbf1da5b9ef7c1044c0beea54acce
Paul <paulallanmain@gmail.com>
2025-04-16 08:13:14 +0300

    Complete lab

---
+++ src/components/Article.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  let emoji = "";
+  
+  if (minutes < 30) {
+    const cups = Math.ceil(minutes / 5);
+    emoji = "☕️".repeat(cups);
+  } else {
+    const bentos = Math.ceil(minutes / 10);
+    emoji = "🍱".repeat(bentos);
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emoji} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
